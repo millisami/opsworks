2066eeb4bb9f390fdf259f8b6dec30b41d4ec5c0
Millisami <millisami@gmail.com>
2013-12-12 14:51:10 +0545

    Added postgresql cookbook

---
+++ postgresql/README.md
@@ -0,0 +1,13 @@
+# aws_tagliners cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Millisami (<millisami@gmail.com>)
---
+++ postgresql/metadata.rb
@@ -0,0 +1,10 @@
+name             'aws_tagliners'
+maintainer       'Millisami'
+maintainer_email 'millisami@gmail.com'
+license          'MIT'
+description      'Installs/Configures aws_tagliners'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends           'build-essential'
+depends           'postgresql'
